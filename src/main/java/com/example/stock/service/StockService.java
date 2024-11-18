f6e3f0e6bec8bd4d94a6948c42e518a237994587
seonhyeokjun <tjsrmatn@naver.com>
2024-11-18 22:16:26 +0900

    Synchronized 이용해보기

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,13 +4,15 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class StockService {
     private final StockRepository stockRepository;
 
-    public void decrease(Long id, Long quantity) {
+    // @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 상품이 없습니다. id=" + id));
         stock.decreaseQuantity(quantity);
 
