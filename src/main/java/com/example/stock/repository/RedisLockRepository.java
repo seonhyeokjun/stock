448841d20366e13c9b041c54cce9fdf997162452
seonhyeokjun <tjsrmatn@naver.com>
2024-11-26 23:02:30 +0900

    Lettuce를 작성하여 재고감소 로직 작성하기

---
+++ src/main/java/com/example/stock/repository/RedisLockRepository.java
@@ -0,0 +1,29 @@
+package com.example.stock.repository;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+public class RedisLockRepository {
+    private RedisTemplate<String, String> redisTemplate;
+
+    public RedisLockRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Boolean lock(long key) {
+        return redisTemplate
+                .opsForValue()
+                .setIfAbsent(generateKey(key), "lock", Duration.ofMillis(3_000));
+    }
+
+    public void unlock(long key) {
+        redisTemplate.delete(generateKey(key));
+    }
+
+    private String generateKey(Long key) {
+        return key.toString();
+    }
+}
